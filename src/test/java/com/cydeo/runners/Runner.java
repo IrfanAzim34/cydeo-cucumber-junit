431969dd5c510f374ce940824623a2d3470c53b2
Irfan Azim <irfanteipov@gmail.com>
2023-02-26 15:09:17 +0300

    Added New Files

--- src/test/java/com/cydeo/runners/Runner.java
+++ src/test/java/com/cydeo/runners/Runner.java
@@ -9,12 +9,12 @@ import org.junit.runner.RunWith;
         plugin = {
                 "html:target/cucumber-reports.html",
                 "rerun:target/rerun.txt",
-                ""
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber"
         },
         features = "src/test/resources/features",
         glue = "com/cydeo/step_definitions",
         dryRun = false,
-        tags = "@wip"
+        tags = "@smoke"
 )
 
 public class Runner {
